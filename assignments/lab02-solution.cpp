739dbdc6132092968bdf5bb63f906e42a64efe22
Derek Harter <derek@harter.pro>
2015-01-23 21:03:22 -0600

    + ‘class/spring2015/csci515/assignments/lab02-solution.cpp’

---
+++ assignments/lab02-solution.cpp
@@ -0,0 +1,34 @@
+/**
+ * @author Derek Harter
+ * @cwid   123 45 678
+ * @class  CSci 515, Spring 2015
+ * @ide    Gnu C++ / Emacs
+ * @date   January 22, 2015
+ * @assg   Lab 02 Averages of Numbers
+ *
+ * @description Write a sentenial controlled loop to
+ *    prompt user to enter a series of integer numbers.
+ *    Perform a series of calculation on the numbers,
+ *    including finding their average, min, max, etc.
+ *    and display the results.
+ */
+#include <iostream>
+using namespace std;
+
+
+/** main entry point
+ * The main entry point for this program.  Execution
+ * of this program will beigin with this function.
+ *
+ * @returns An int value.  By default, if we don't specify a return or
+ *           exit value, 0 is returned to indicate successful program
+ *           completion.  A non-zero value indicates an error or
+ *           problem with execution.
+ */
+int main()
+{
+  cout << "Hello, world!" << endl;
+
+  // return 0 to indicate successful completion
+  return 0;
+}
