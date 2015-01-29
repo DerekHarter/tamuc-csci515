105bb225400b90d7dbf01ac48c349f5eb4ae44d2
Derek Harter <derek@harter.pro>
2015-01-29 12:33:32 -0600

    + ‘class/spring2015/csci515/assignments/lab-03-solution.cpp’

---
+++ assignments/lab-03-solution.cpp
@@ -0,0 +1,31 @@
+/**
+ * @author Joe Programmer
+ * @cwid   123 45 678
+ * @class  CSci 515, Spring 2015
+ * @ide    Visual Studio Express 2010
+ * @date   January 1, 2015
+ * @assg   ExamplAssg00
+ *
+ * @description A short descrption of the program and its purpose, and
+ *    the approach you took in solving the problem.
+ */
+#include <iostream>
+using namespace std;
+
+
+/** main entry point
+ * The main entry point for this program.  Execution
+ * of this program will beigin with this function.
+ *
+ * @returns An int value.  By default, if we don't specify a return or
+ *           exit value, 0 is returned to indicate successful program
+ *           completion.  A non-zero value indicates an error or
+ *           problem with execution.
+ */
+int main()
+{
+  cout << "Hello, world!" << endl;
+
+  // return 0 to indicate successful completion
+  return 0;
+}
