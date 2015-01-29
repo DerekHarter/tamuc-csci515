e2cfe07471a0d6b800d80558b9e358bc96bdf1a4
Derek Harter <derek@harter.pro>
2015-01-29 15:51:51 -0600

    / ‘class/spring2015/csci515/assignments/lab-03-data.csv’ / ‘class/spring2015/csci515/assignments/lab-03-gen-data.py’

--- assignments/lab-03-gen-data.py
+++ assignments/lab-03-gen-data.py
@@ -5,7 +5,7 @@ import numpy as np
 NUM_LINES = 10
 NUM_FEATURES = 5
 
-print "%d% % NUM_FEATURES"
+print "%d" % NUM_FEATURES
 for idx in range(1, NUM_LINES+1):
     features = np.random.normal(0.0, 5.0, (NUM_FEATURES,))
     for f in features:
