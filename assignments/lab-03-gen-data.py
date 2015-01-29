5a9cf0a017442439bd1fbdba3f02ddb5d7b5777c
Derek Harter <derek@harter.pro>
2015-01-29 16:15:55 -0600

    / ‘class/spring2015/csci515/assignments/lab-03-gen-data.py’

--- assignments/lab-03-gen-data.py
+++ assignments/lab-03-gen-data.py
@@ -2,6 +2,7 @@
 """Generate a suitable set of data for the Lab 03 assignment.
 """
 import numpy as np
+import sys
 NUM_LINES = 10
 NUM_FEATURES = 5
 
@@ -9,5 +10,5 @@ print "%d" % NUM_FEATURES
 for idx in range(1, NUM_LINES+1):
     features = np.random.normal(0.0, 5.0, (NUM_FEATURES,))
     for f in features:
-        print("%f\t" % f, end="")
-    print ""
+        sys.stdout.write("%f\t" % f)
+    sys.stdout.write("\n")
