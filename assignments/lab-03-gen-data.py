f40f6212470d2ba2b66470ef3b72f9a9e8ac3439
Derek Harter <derek@harter.pro>
2015-01-29 15:46:02 -0600

    / ‘class/spring2015/csci515/assignments/lab-03-gen-data.py’

--- assignments/lab-03-gen-data.py
+++ assignments/lab-03-gen-data.py
@@ -5,7 +5,7 @@ import numpy as np
 NUM_LINES = 10
 NUM_FEATURES = 5
 
-x, y, z = np.random.normal(0.0, 2.0, (NUM_FEATURES,))
+x, y, z = np.random.normal(0.0, 5.0, (NUM_FEATURES,))
 for idx in range(1, NUM_LINES+1):
     print "%d\t%f\t%f\t%f" % (idx, x, y, z)
     xd, yd, zd = np.random.normal(0.0, 0.025, (3,))
