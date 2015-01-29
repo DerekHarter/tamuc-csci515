cb633759d8822b31850bd06c012efeeef6405960
Derek Harter <derek@harter.pro>
2015-01-29 15:37:31 -0600

    / ‘class/spring2015/csci515/assignments/lab-03-gen-data.py’

--- assignments/lab-03-gen-data.py
+++ assignments/lab-03-gen-data.py
@@ -8,7 +8,6 @@ x, y, z = np.random.normal(0.0, 2.0, (3,))
 for idx in range(1, NUM_LINES+1):
     print "%d\t%f\t%f\t%f" % (idx, x, y, z)
     xd, yd, zd = np.random.normal(0.0, 0.025, (3,))
-    print xd, yd, zd
     x += xd
     y += yd
     z += zd
