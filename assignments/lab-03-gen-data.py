7d353c29cb872def9cdddb9a3a7bf56afbf2d24b
Derek Harter <derek@harter.pro>
2015-01-29 15:37:06 -0600

    / ‘class/spring2015/csci515/assignments/lab-03-gen-data.py’

--- assignments/lab-03-gen-data.py
+++ assignments/lab-03-gen-data.py
@@ -4,10 +4,10 @@
 import numpy as np
 NUM_LINES = 10
 
-x, y, z = np.random.randn(0.0, 2.0, (3,))
+x, y, z = np.random.normal(0.0, 2.0, (3,))
 for idx in range(1, NUM_LINES+1):
     print "%d\t%f\t%f\t%f" % (idx, x, y, z)
-    xd, yd, zd = np.random.randn(0.0, 0.25, (3,))
+    xd, yd, zd = np.random.normal(0.0, 0.25, (3,))
     print xd, yd, zd
     x += xd
     y += yd
