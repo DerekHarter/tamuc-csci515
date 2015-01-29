83e71d9d6581646fd68bcd052c67758753ede8ab
Derek Harter <derek@harter.pro>
2015-01-29 15:32:11 -0600

    / ‘class/spring2015/csci515/assignments/lab-03-gen-data.py’

--- assignments/lab-03-gen-data.py
+++ assignments/lab-03-gen-data.py
@@ -6,4 +6,7 @@ NUM_LINES = 10
 
 x, y, z = np.random.randn(3)
 for idx in range(1, NUM_LINES+1):
-    print idx, x, y, z
+    print "%d\t%f\t$f\t%f" % (idx, x, y, z)
+    x += np.random.randn(1, 0.0, 0.25)
+    y += np.random.randn(1, 0.0, 0.25)
+    z += np.random.randn(1, 0.0, 0.25)
