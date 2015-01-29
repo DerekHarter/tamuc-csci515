18cd04a00b1f1d4fe2eac101bacc1755dd43caae
Derek Harter <derek@harter.pro>
2015-01-29 13:45:56 -0600

    / ‘class/spring2015/csci515/assignments/lab-03-solution.cpp’

--- assignments/lab-03-solution.cpp
+++ assignments/lab-03-solution.cpp
@@ -39,7 +39,7 @@ int main()
 
   while (!inputFile.eof())
   {
-    inputFile >> idx >> "," >> f1 >> "," >> f2 >> "," >> f3;
+    inputFile >> idx >> f1 >> f2 >> f3;
     cout << "Found the following: idx=" << idx << " " << f1 << " " << f2 << " " << f3 << endl;
   }
 
