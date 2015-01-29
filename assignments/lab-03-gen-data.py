e111b913c517faf454fc28e476e9fd23285a9645
Derek Harter <derek@harter.pro>
2015-01-29 16:13:42 -0600

    / ‘class/spring2015/csci515/assignments/lab-03-gen-data.py’

--- assignments/lab-03-gen-data.py
+++ assignments/lab-03-gen-data.py
@@ -9,5 +9,5 @@ print "%d" % NUM_FEATURES
 for idx in range(1, NUM_LINES+1):
     features = np.random.normal(0.0, 5.0, (NUM_FEATURES,))
     for f in features:
-        print "%f\t" % f,
+        print("%f\t" % f, end="")
     print ""
