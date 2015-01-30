9cb4c91fb8e45b89e9e1d5f667666cf4cc6544a6
Derek Harter <derek@harter.pro>
2015-01-30 11:22:20 -0600

    / ‘class/spring2015/csci515/assignments/assg-03-gen-data.py’

--- assignments/assg-03-gen-data.py
+++ assignments/assg-03-gen-data.py
@@ -7,8 +7,12 @@ NUM_LINES = 50
 CLASSES = ['UP', 'DOWN', 'STRANGE', 'CHARM']
 CHOSEN_CLASS = 'STRANGE'
 
-x, y, z = np.random.normal(0.0, 5.0, (3,))
-print x,y,z
+x_prev, y_prev, z_prev = np.random.normal(0.0, 5.0, (3,))
+for trial in range(1, NUM_LINES+1):
+    trial_class = np.random.choice(CLASSES)
+
+    printf "%03d\t%5s" % (trial, trial_class)
+
 # for idx in range(1, NUM_LINES+1):
 #     features = np.random.normal(0.0, 5.0, (NUM_FEATURES,))
 #     for f in features:
