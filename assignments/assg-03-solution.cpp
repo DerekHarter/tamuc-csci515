e3a9f02a92ea56664bcfc42e2ac35ab3c6f63bc3
Derek Harter <derek@harter.pro>
2015-01-30 12:09:50 -0600

    / ‘class/spring2015/csci515/assignments/assg-03-solution.cpp’

--- assignments/assg-03-solution.cpp
+++ assignments/assg-03-solution.cpp
@@ -29,6 +29,10 @@ int main()
   string inputFileName = "assg-03-data.dsv";
   ifstream inputFile(inputFileName.c_str());
   string filterClass;
+  int trial;
+  float x_prev, y_prev, z_prev;  // hold previous values of position
+  float x, y, z; // will hold current values of position
+  bool firstTrial = true; // use a flag to handle first item, before we have 2 items to calc distance with 
   
   // test we found and opened file successfully
   if (!inputFile)
