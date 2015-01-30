4c121ab46b6e52e1d15653b8c19e42cb18b70c93
Derek Harter <derek@harter.pro>
2015-01-30 12:00:53 -0600

    / ‘class/spring2015/csci515/assignments/assg-03-solution.cpp’

--- assignments/assg-03-solution.cpp
+++ assignments/assg-03-solution.cpp
@@ -26,14 +26,8 @@ using namespace std;
  */
 int main()
 {
-  string inputFileName = "lab-03-data.dsv";
+  string inputFileName = "assg-03-data.dsv";
   ifstream inputFile(inputFileName.c_str());
-  int numFeatures;
-  int lineNumber;
-  float value1, value2, value3;
-  float maxValue;
-  float minValue;
-  float sum;
 
   // test we found and opened file successfully
   if (!inputFile)
@@ -43,45 +37,9 @@ int main()
   }
 
   // now process all of the lines of data
-  lineNumber = 1;
-  minValue = 1000.0;
-  maxValue = -1000.0;
-  cout << setw(5) << "Trial"
-       << setw(15) << "Feature-1"
-       << setw(15) << "Feature-2"
-       << setw(15) << "Feature-3" << endl;
   while (inputFile >> value1 >> value2 >> value3)
   {
-    // reformat the input nicely
-    cout << setw(5) << lineNumber
-	 << scientific << setw(15) << setprecision(5) << value1
-	 << scientific << setw(15) << setprecision(5) << value2
-	 << scientific << setw(15) << setprecision(5) << value3
-         << endl;
-
-    // keep track of minimum, maximum and running sum so we can compute
-    // average
-    if (value1 < minValue)
-    {
-      minValue = value1;
-    }
-    if (value2 > maxValue)
-    {
-      maxValue = value2;
-    }
-    sum += value3;
-    lineNumber++;
   }
-
-  // display summary information:
-  cout << endl;
-  cout << setw(25) << right << "Number of Trials:" << setw(10) << lineNumber - 1 << endl;
-  cout << setw(25) << right << "Minimum of Feature 1:" << setw(10)
-       << fixed << setprecision(5) << minValue << endl;
-  cout << setw(25) << right << "Maximum of Feature 2:" << setw(10)
-       << fixed << setprecision(5) << maxValue << endl;
-  cout << setw(25) << right << "Average of Feature 3:" << setw(10)
-       << fixed << setprecision(5) << ( sum / (float)(lineNumber - 1) ) << endl;
 
 
   // return 0 to indicate successful completion
