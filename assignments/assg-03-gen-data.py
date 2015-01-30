8635fd02bacae546956cbfb03e00a7e37e44e0a2
Derek Harter <derek@harter.pro>
2015-01-30 11:25:15 -0600

    / ‘class/spring2015/csci515/assignments/assg-03-gen-data.py’

--- assignments/assg-03-gen-data.py
+++ assignments/assg-03-gen-data.py
@@ -11,6 +11,14 @@ x_prev, y_prev, z_prev = np.random.normal(0.0, 5.0, (3,))
 for trial in range(1, NUM_LINES+1):
     trial_class = np.random.choice(CLASSES)
 
+    if trial_class == CHOSEN_CLASS:
+        x_d, y_d, z_d = np.random.normal(0.0, 0.25, (3,))
+        x = x_prev + x_d
+        y = y_prev + y_d
+        z = z_prev + z_d
+    else:
+        x, y, z = np.random.normal(0.0, 5.0, (3,))
+        
     print "%03d\t%-s" % (trial, trial_class)
 
 # for idx in range(1, NUM_LINES+1):
