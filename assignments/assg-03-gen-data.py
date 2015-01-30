24a31cd9b9e93aa47681b3d3a26d5b38ee252d69
Derek Harter <derek@harter.pro>
2015-01-30 11:27:50 -0600

    / ‘class/spring2015/csci515/assignments/assg-03-gen-data.py’

--- assignments/assg-03-gen-data.py
+++ assignments/assg-03-gen-data.py
@@ -21,11 +21,5 @@ for trial in range(1, NUM_LINES+1):
 
     print "%03d\t%8.5f\t%8.5f\t%8.5f\t%-s" % (trial, x, y, z, trial_class)
 
-# for idx in range(1, NUM_LINES+1):
-#     features = np.random.normal(0.0, 5.0, (NUM_FEATURES,))
-#     for f in features:
-#         prec = np.random.randint(3, 10, size=(1,))
-#         width = prec + np.random.randint(2, 5, size=(1,))
-#         fmt = "%%%d.%df\t" % (width, prec)
-#         sys.stdout.write(fmt % f)
-#     sys.stdout.write("\n")
+    if trial_class == CHOSEN_CLASS:
+        x_prev, y_prev, z_prev = x, y, z
