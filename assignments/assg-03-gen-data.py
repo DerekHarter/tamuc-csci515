fb7bb3dd6396918106a574120a7fa7f45fa8145d
Derek Harter <derek@harter.pro>
2015-01-30 11:19:05 -0600

    / ‘class/spring2015/csci515/assignments/assg-03-gen-data.py’

--- assignments/assg-03-gen-data.py
+++ assignments/assg-03-gen-data.py
@@ -5,7 +5,10 @@ import numpy as np
 import sys
 NUM_LINES = 50
 CLASSES = ['UP', 'DOWN', 'STRANGE', 'CHARM']
+CHOSEN_CLASS = 'STRANGE'
 
+x, y, z = NP.RANDOM.NORMAL(0.0, 5.0, (3,))
+print x,y,z
 for idx in range(1, NUM_LINES+1):
     features = np.random.normal(0.0, 5.0, (NUM_FEATURES,))
     for f in features:
