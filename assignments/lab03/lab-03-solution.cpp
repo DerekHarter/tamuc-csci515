a6e3d5aebb2d8f060a2d19410a5ca2288324e603
Derek Harter <derek@harter.pro>
2015-02-03 19:22:30 -0600

    / ‘class/spring2015/csci515/assignments/lab03/lab-03-solution.cpp’

--- assignments/lab03/lab-03-solution.cpp
+++ assignments/lab03/lab-03-solution.cpp
@@ -6,12 +6,14 @@
  * @date   January 30, 2015
  * @assg   Lab 03
  *
- * @description An example solution for the Lab 03.  We rely on the IOStream tokenizing
- *     behavior on whitespace in order to process our lines.  Our while loop reads in
- *     all of the values for a single line of input, and works because the logical
- *     result of performing such input will be true if it succeeded, and false if it doesn't
- *     (once we read past end of file).  We (re)format our input nicely to output stream,
- *     using standard IOStream manipulators to do the formatting.
+ * @description An example solution for the Lab 03.  We rely on the
+ *     IOStream tokenizing behavior on whitespace in order to process
+ *     our lines.  Our while loop reads in all of the values for a
+ *     single line of input, and works because the logical result of
+ *     performing such input will be true if it succeeded, and false
+ *     if it doesn't (once we read past end of file).  We (re)format
+ *     our input nicely to output stream, using standard IOStream
+ *     manipulators to do the formatting.
  */
 #include <iostream>
 #include <fstream>
