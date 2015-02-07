91fc41191c0015e9c305e76b04683fdfcc8b19c8
Derek Harter <derek@harter.pro>
2015-02-06 20:56:57 -0600

    + ‘class/spring2015/csci515/lectures/lect04/P04-function-call-stack.cpp’

---
+++ lectures/lect04/P04-function-call-stack.cpp
@@ -0,0 +1,46 @@
+/**
+ * @author Derek Harter
+ * @cwid   123 45 678
+ * @class  CSci 515, Spring 2015
+ * @ide    Emacs, GCC/G++
+ * @date   February 5, 2015
+ * @assg   Lecture 04, P02 User Defined Functions
+ *
+ * @description Lets define some of our own functions.
+ */
+#include <iostream>
+using namespace std;
+
+
+/** A
+ * The function A
+ */
+int A(int p)
+{
+
+  if (a < 2)
+  {
+    B();
+  }
+
+  cout << a << endl;
+  
+  return 0;
+}
+
+
+/** main entry point
+ * The main entry point for this program.  Execution
+ * of this program will begin with this function.
+ *
+ * @returns An int value.  By default, if we don't specify a return or
+ *           exit value, 0 is returned to indicate successful program
+ *           completion.  A non-zero value indicates an error or
+ *           problem with execution.
+ */
+int main()
+{
+
+  // Return 0 to indicate successful completion
+  return 0;
+}
