0471f3d5e45bffec4da29c3a47f321cf7d7a2ef5
Derek Harter <derek@harter.pro>
2015-02-11 20:06:39 -0600

    / ‘class/spring2015/csci515/assignments/lab05/lab-05-dice-game-solution.cpp’

--- assignments/lab05/lab-05-dice-game-solution.cpp
+++ assignments/lab05/lab-05-dice-game-solution.cpp
@@ -26,29 +26,6 @@ using namespace std;
  */
 bool isPerfectNumber(int number)
 {
-  int i;
-  int sum = 0;
-  
-  // loop through all values from 1 to number-1 to find divisors
-  for (i = 1; i <= number - 1; i++)
-  {
-    // whenever we find a divisor, add it to the running sum
-    if (number % i == 0)
-    {
-      sum += i;
-    }
-  }
-
-  // if all the divisors summed up to the original number
-  // then it is a perfect number
-  if (sum == number)
-  {
-    return true;
-  }
-  else
-  {
-    return false;
-  }
 }
 
 
@@ -64,16 +41,6 @@ bool isPerfectNumber(int number)
  */
 void findPerfectNumbers(int n)
 {
-  int i;
-
-  // loop through the range we are to search over, looking for perfect numbers
-  for (i = 1; i <= n; i++)
-  {
-    if (isPerfectNumber(i))
-    {
-      cout << i << " is a perfect number" << endl;
-    }
-  }
 }
 
 
@@ -91,14 +58,6 @@ int main()
 {
   int n;
 
-  // ask user for input
-  cout << "This program finds all of the perfect numbers in the range from 1 to N." << endl;
-  cout << "Up to what value of N should I search for perfect numbers: ";
-  cin >> n;
-
-  // search for perfect numbers
-  findPerfectNumbers(n);
-  
   // clean up and return 0 to indicate successful completion
   return 0;
 }
