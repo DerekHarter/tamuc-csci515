255d338095e466a5c34a0623b882a9c4ddfa1e16
Derek Harter <derek@harter.pro>
2015-02-13 13:42:56 -0600

    + ‘class/spring2015/csci515/lectures/lect05/P04-recursion.cpp’

---
+++ lectures/lect05/P04-recursion.cpp
@@ -0,0 +1,26 @@
+/**
+ * @author Derek Harter
+ * @cwid   123 45 678
+ * @class  CSci 515, Spring 2015
+ * @ide    Emacs, GCC/G++
+ * @date   February 5, 2015
+ * @assg   Lecture 04, P02 User Defined Functions
+ *
+ * @description Example of using the c standard library random number generator functions.
+ */
+#include <iostream>
+using namespace std;
+
+
+/** main entry point
+ * The main entry point for this program.  Execution
+ * of this program will begin with this function.
+ *
+ * @returns An int value.  By default, if we don't specify a return or
+ *           exit value, 0 is returned to indicate successful program
+ *           completion.  A non-zero value indicates an error or
+ *           problem with execution.
+ */
+int main()
+{
+}
