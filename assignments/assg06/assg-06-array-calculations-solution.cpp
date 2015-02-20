93675052478bcf1d0b30fa9fae5bec671dd2eec7
Derek Harter <derek@harter.pro>
2015-02-19 19:10:48 -0600

    / ‘class/spring2015/csci515/assignments/assg06/assg-06-array-calculations-solution.cpp’

--- assignments/assg06/assg-06-array-calculations-solution.cpp
+++ assignments/assg06/assg-06-array-calculations-solution.cpp
@@ -172,8 +172,9 @@ int main()
   readArrayFromFile(values, NUM_VALUES, INPUT_FILE_NAME);
 
   // display the results from processing our array of values
-  cout << "Minimum: " << setw(10) << fixed << setprecision(8) << findMinimumValue(values, NUM_VALUES) << endl;
-  cout << "Maximum: " << setw(10) << fixed << setprecision(8) << findMaximumValue(values, NUM_VALUES) << endl;
+  cout << "Minimum: " << setw(12) << fixed << setprecision(8) << findMinimumValue(values, NUM_VALUES) << endl;
+  cout << "Maximum: " << setw(12) << fixed << setprecision(8) << findMaximumValue(values, NUM_VALUES) << endl;
+  cout << "Sum:     " << setw(12) << fixed << setprecision(8) << findMaximumValue(values, NUM_VALUES) << endl;
   
   //  Return 0 to indicate program terminated successfully
   return 0;
