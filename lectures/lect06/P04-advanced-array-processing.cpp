93f76aca49d99a4bf9b0f724fa71bb5bde3ba8ce
Derek Harter <derek@harter.pro>
2015-02-20 19:46:25 -0600

    + ‘class/spring2015/csci515/lectures/lect06/P04-advanced-array-processing.cpp’

---
+++ lectures/lect06/P04-advanced-array-processing.cpp
@@ -0,0 +1,25 @@
+/**
+ * @author Derek Harter
+ * @cwid   123 45 678
+ * @class  CSci 515, Spring 2015
+ * @ide    Emacs, GCC/G++
+ * @date   February 5, 2015
+ * @assg   Lecture 06, P04 Advanced Array Processing
+ *
+ * @description Some examples of some more advanced array processing.
+ */
+#include <iostream>
+using namespace std;
+
+/** main entry point
+ * The main entry point for this program.  Execution
+ * of this program will begin with this function.
+ *
+ * @returns An int value.  By default, if we don't specify a return or
+ *           exit value, 0 is returned to indicate successful program
+ *           completion.  A non-zero value indicates an error or
+ *           problem with execution.
+ */
+int main()
+{
+}
