74509d8ffa49eed1a15fa0f9bbd74f289f41d8ed
Derek Harter <derek@harter.pro>
2015-02-22 14:35:41 -0600

    / ‘class/spring2015/csci515/lectures/lect06/P05-advanced-array-processing.cpp’ / ‘class/spring2015/csci515/lectures/lect06/p05’

--- lectures/lect06/P05-advanced-array-processing.cpp
+++ lectures/lect06/P05-advanced-array-processing.cpp
@@ -217,7 +217,7 @@ void displayArrayHistogram(int values[], int size)
 int main()
 {
   // Array to hold a series of simulated random dice throws
-  const int NUM_TRIALS = 3;
+  const int NUM_TRIALS = 100;
   int dice1[NUM_TRIALS];
   int dice2[NUM_TRIALS];
 
