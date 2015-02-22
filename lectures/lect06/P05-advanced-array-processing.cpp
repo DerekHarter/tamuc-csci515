0b59dd9f40d4aed8731d60cb80b8b94887058efb
Derek Harter <derek@harter.pro>
2015-02-22 13:46:31 -0600

    / ‘class/spring2015/csci515/lectures/lect06/P05-advanced-array-processing.cpp’

--- lectures/lect06/P05-advanced-array-processing.cpp
+++ lectures/lect06/P05-advanced-array-processing.cpp
@@ -6,7 +6,10 @@
  * @date   February 5, 2015
  * @assg   Lecture 06, P04 Advanced Array Processing
  *
- * @description Some examples of some more advanced array processing.
+ * @description Some examples of some more advanced array processing.  The dice throwing
+ *   game simulation.  Simulate throwing a pair of dice.  Count up the number of the sums
+ *   we observe.  Display as a crude histogram.  Show example of a normal distribution as the
+ *   result of many small uniform distributions that are summed.
  */
 #include <iostream>
 #include <iomanip>
