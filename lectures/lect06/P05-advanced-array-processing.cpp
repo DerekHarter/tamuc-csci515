fce26f81bac1c7b330b7a6241adc64719aae39b0
Derek Harter <derek@harter.pro>
2015-02-22 14:01:35 -0600

    / ‘class/spring2015/csci515/lectures/lect06/P05-advanced-array-processing.cpp’

--- lectures/lect06/P05-advanced-array-processing.cpp
+++ lectures/lect06/P05-advanced-array-processing.cpp
@@ -169,4 +169,8 @@ int main()
   cout << "---- Sum of the two dice 3 for N trials" << endl;
   displayArray(diceSum, NUM_TRIALS);
 
+  // our first question, what are the counts of each possible sum 2-12?
+  const int MAX_ROLL = 13; // 12 - 1, since arrays are indexed up to size+1, we don't use index 0 or 1
+  int rollFrequency[MAX_ROLL];
+  countFrequency(diceSum, NUM_TRIALS, rollFrequency);
 }
