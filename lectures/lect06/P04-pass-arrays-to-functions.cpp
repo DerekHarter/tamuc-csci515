c74da30d636fdfb9a482b5396b973f07ff2a663d
Derek Harter <derek@harter.pro>
2015-02-22 13:13:21 -0600

    / ‘class/spring2015/csci515/lectures/lect06/P04-pass-arrays-to-functions.cpp’

--- lectures/lect06/P04-pass-arrays-to-functions.cpp
+++ lectures/lect06/P04-pass-arrays-to-functions.cpp
@@ -65,7 +65,8 @@ void displayArray(int values[], int size)
 {
   for (int idx = 0; idx < size; idx++)
   {
-    values[idx] = 0;
+    cout << "values[" << setw(3) << setfill('0') << right << idx << "] = "
+	 << values[ids] << endl;
   }
 }
 
