77982ff6ebeb9d8f0fa801ecaa69cb5c7198b3f0
Derek Harter <derek@harter.pro>
2015-02-22 13:28:48 -0600

    / ‘class/spring2015/csci515/lectures/lect06/P04-pass-arrays-to-functions.cpp’

--- lectures/lect06/P04-pass-arrays-to-functions.cpp
+++ lectures/lect06/P04-pass-arrays-to-functions.cpp
@@ -189,7 +189,7 @@ int main()
 
   // initialize array to random value
   initArrayToRandomRange(values, ARRAY_SIZE, 1, 6);
-  srand(time(0));
+  srand(time(0));  // seed random generate with time in ms, e.g. a random starting point
   cout << "--------- initArrayToRandomRange()" << endl;
   displayArray(values, ARRAY_SIZE);
   cout << endl;
