350d688c89003bbd13c44c26ff5ca4a2ca96c34e
Derek Harter <derek@harter.pro>
2015-02-22 18:06:47 -0600

    / ‘class/spring2015/csci515/lectures/lect06/P06-advanced-array-processing.cpp’

--- lectures/lect06/P06-advanced-array-processing.cpp
+++ lectures/lect06/P06-advanced-array-processing.cpp
@@ -222,7 +222,7 @@ void calculateFrequencyProbabilityOfArray(int inputArray[], int size,
   }
 
   // normalize all frequencies to a probability value in range [0.0, 1.0]
-  for (int idx = 0; idx < maxValue; idx++)
+  for (int idx = 0; idx < maxValue+1; idx++)
   {
     probabilityArray[idx] = probabilityArray[idx] / (float)size;
   }
