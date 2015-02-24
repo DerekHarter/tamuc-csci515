14db4d9c4f53d289d38c735e86711050e3ffc5bf
Derek Harter <derek@harter.pro>
2015-02-24 14:39:10 -0600

    / ‘class/spring2015/csci515/lectures/lect07/gen-exp-data.py’

--- lectures/lect07/gen-exp-data.py
+++ lectures/lect07/gen-exp-data.py
@@ -8,7 +8,7 @@ NUM_FEATURES = 3
 
 #print "%d" % NUM_FEATURES
 for idx in range(1, NUM_LINES+1):
-    features = np.random.normal(0.0, 5.0, (NUM_FEATURES,))
+    features = np.random.normal(0.0, 0.1, (NUM_FEATURES,))
     for f in features:
         print "%0.8f" % f,
     print ""
