05021599dcacb442d5dc69807480bc3f628feb6c
Derek Harter <derek@harter.pro>
2015-02-24 15:29:49 -0600

    + ‘class/spring2015/csci515/lectures/lect07/P03-array-shifting-moving.cpp’ / ‘class/spring2015/csci515/lectures/lect07/p02’

---
+++ lectures/lect07/P03-array-shifting-moving.cpp
@@ -0,0 +1,13 @@
+/**
+ * @author Derek Harter
+ * @cwid   123 45 678
+ * @class  CSci 515, Spring 2015
+ * @ide    Emacs, GCC/G++
+ * @date   February 5, 2015
+ * @assg   Lecture 07, P02 Processing 2-D arrays using functions
+ *
+ * @description Examples of more advanced processing of 2-D arrays
+ *   using functions to read from files, and calculated and display
+ *   results.
+ */
+#include <iostream>
