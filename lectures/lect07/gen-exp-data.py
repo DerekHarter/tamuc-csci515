024254cf95f5f56540db09c1556383023992c38a
Derek Harter <derek@harter.pro>
2015-02-24 14:42:27 -0600

    / ‘class/spring2015/csci515/lectures/lect07/gen-exp-data.py’

--- lectures/lect07/gen-exp-data.py
+++ lectures/lect07/gen-exp-data.py
@@ -7,8 +7,11 @@ NUM_LINES = 50
 NUM_FEATURES = 3
 
 #print "%d" % NUM_FEATURES
+x,y,z = np.random.normal(0.0, 1.0, (NUM_FEATURES,))
 for idx in range(1, NUM_LINES+1):
-    features = np.random.normal(0.0, 0.1, (NUM_FEATURES,))
-    for f in features:
-        print "%10.8f" % f,
-    print ""
+    xd,yd,zd = np.random.normal(0.0, 0.05, (NUM_FEATURES,))
+    x += xd
+    y += yd
+    z += zd
+    print "%12.8f %12.8f %12.8f" % (x, y, z)
+
