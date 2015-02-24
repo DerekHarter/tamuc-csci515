eb631375767d73a8d985720842f5608fc9d12181
Derek Harter <derek@harter.pro>
2015-02-24 13:56:46 -0600

    + ‘class/spring2015/csci515/lectures/lect07/P01-2-dim-arrays.cpp’

---
+++ lectures/lect07/P01-2-dim-arrays.cpp
@@ -0,0 +1,61 @@
+/**
+ * @author Derek Harter
+ * @cwid   123 45 678
+ * @class  CSci 515, Spring 2015
+ * @ide    Emacs, GCC/G++
+ * @date   February 5, 2015
+ * @assg   Lecture 06, P01 Array declarations, indexing, initialization
+ *
+ * @description Examples of declaring and initializing an array.  Discussion of array indexing
+ *    and conventions for (safely) accessing/declaring arrays.
+ */
+#include <iostream>
+using namespace std;
+
+/** main entry point
+ * The main entry point for this program.  Execution
+ * of this program will begin with this function.
+ *
+ * @returns An int value.  By default, if we don't specify a return or
+ *           exit value, 0 is returned to indicate successful program
+ *           completion.  A non-zero value indicates an error or
+ *           problem with execution.
+ */
+int main()
+{
+  // Declare an array
+  int c[10];
+
+  // index an array
+  c[0] = 5;
+  c[1] = 10;
+  c[10] = 22; // ERROR access beyond bounds of array, NO BAD PROGRAMMER
+
+  // loop to initialize an array
+  // always, ALWAYS, ALWAYS index starting at 0, up to (but not including)
+  // the max value.  Arrays in c are always index from 0 to N-1
+  for (int idx = 0; idx < 10; idx++)
+  {
+    c[idx] = 0;
+  }
+
+  // Can use an initializer list when declaring arrays, though not common
+  // in real programs
+  int d[5] = {0, 1, 2, 3, 4};
+
+  // Basic loop to access first 5 values in c and d
+  for (int idx = 0; idx < 5; idx++)
+  {
+    cout << "c[" << idx << "] = " << c[idx] << " "
+	 << "d[" << idx << "] = " << d[idx] << endl;
+  }
+
+  // ERROR ERROR Danger Will Robinson!
+  // Its is very bad to try and access past the end of an array
+  for (int idx = 0; idx < 10; idx++)
+  {
+    cout << "c[" << idx << "] = " << c[idx] << " "
+	 << "d[" << idx << "] = " << d[idx] << endl;
+  }
+
+}
