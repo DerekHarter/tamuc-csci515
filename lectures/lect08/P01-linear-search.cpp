1dd65e7b455444e67784ee8199a9ee9015f6d01b
Derek Harter <derek@harter.pro>
2015-03-13 15:25:02 -0500

    / ‘class/spring2015/csci515/lectures/lect08/P01-linear-search.cpp’

--- lectures/lect08/P01-linear-search.cpp
+++ lectures/lect08/P01-linear-search.cpp
@@ -83,6 +83,29 @@ void displayArray(int values[], int size)
 }
 
 
+/** Linear Search
+ * Perform a linear search of an array of integers.  We search for the indicated key
+ * and return the index where whe find that key.
+ *
+ * @param values An array of integers.  Arrays are passed by value
+ *   so when we initialize this array to random values, the
+ *   initialized values will be passed to the caller.
+ * @param size int The size of the array to initialize.
+ *
+ * @returns void.  However, as a side effect we display the
+ *   values in the array to standard output.
+ */
+void displayArray(int values[], int size)
+{
+  for (int idx = 0; idx < size; idx++)
+  {
+    cout << setw(3) << setfill('0') << idx << ": "
+	 << setw(3) << setfill(' ') << values[idx]
+	 << endl;
+  }
+}
+
+
 /** main entry point
  * The main entry point for this program.  Execution
  * of this program will begin with this function.
@@ -105,7 +128,6 @@ int main()
   // Let the user search the array:
   int key = 0;
   int index;
-  
   while (key != -1)
   {
     // get a value to search for
