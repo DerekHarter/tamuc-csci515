cbf5fee3a258007170bcdf34c4620d8ea8225fe6
Derek Harter <derek@harter.pro>
2015-03-25 14:57:49 -0500

    / ‘class/spring2015/csci515/assignments/assg08/assg-08-selection-sort-solution.cpp’

--- assignments/assg08/assg-08-selection-sort-solution.cpp
+++ assignments/assg08/assg-08-selection-sort-solution.cpp
@@ -4,10 +4,16 @@
  * @class  CSci 515, Spring 2015
  * @ide    Gnu C/C++, Emacs
  * @date   February 19, 2015
- * @assg   Assg 07
+ * @assg   Assg 08
  *
- * @description Implement function to search for and remove duplicates from an
- *   array.
+ * @description Implement a selection sort.  A selection sort works
+ *   by finding the minimum value in an unsorted subportion of an
+ *   array and then swapping this value to the correct location in
+ *   the sorted portion of the array.  This implementation creates two
+ *   helper functions to perform a lot of the work of the selection
+ *   sort.  One function performs the search for the minimum value
+ *   in portions of an array.  The other helper function swaps two
+ *   locations in an array.
  */
 #include <iostream>
 #include <iomanip>
