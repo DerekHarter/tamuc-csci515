4b8e8b7da692392d367e6b6180d07f6bc8975085
Derek Harter <derek@harter.pro>
2015-04-07 16:19:08 -0500

    / ‘class/spring2015/csci515/assignments/lab10/lab-10-deck-of-cards-solution.cpp’

--- assignments/lab10/lab-10-deck-of-cards-solution.cpp
+++ assignments/lab10/lab-10-deck-of-cards-solution.cpp
@@ -14,6 +14,10 @@
 using namespace std;
 
 
+// User defined Card type
+enum Suit {SPADES, CLUBS, DIAMONDS, HEARTS};
+enum Face {};
+
 /** main entry point
  * The main entry point for this program.  Execution
  * of this program will begin with this function.
@@ -25,35 +29,4 @@ using namespace std;
  */
 int main()
 {
-  // test V1 bubble sort optimizations
-  const int NUM_VALUES = 8;
-  int almostSorted1[NUM_VALUES] = {3, 5, 13, 9, 18, 11, 10, 15};
-
-  cout << "Before calling bubbleSortV1:" << endl;
-  displayArray(almostSorted1, NUM_VALUES);
-  cout << endl;
-
-  bubbleSortV1(almostSorted1, NUM_VALUES);
-  cout << endl;
-
-  cout << "After calling bubbleSortV1:" << endl;
-  displayArray(almostSorted1, NUM_VALUES);
-  cout << endl;
-
-  // test V2 bubble sort optimizations
-  int almostSorted2[NUM_VALUES] = {7, 5, 3, 9, 10, 11, 15, 18};
-
-  cout << "Before calling bubbleSortV2:" << endl;
-  displayArray(almostSorted2, NUM_VALUES);
-  cout << endl;
-
-  bubbleSortV2(almostSorted2, NUM_VALUES);
-  cout << endl;
-
-  cout << "After calling bubbleSortV2:" << endl;
-  displayArray(almostSorted2, NUM_VALUES);
-  cout << endl;
-
-  // return 0 to indicate successful completion.
-  return 0;
 }
