bacca820855e86283e49260e1d93e51b71a4d6a9
Derek Harter <derek@harter.pro>
2015-04-07 13:45:52 -0500

    + ‘class/spring2015/csci515/lectures/lect10/P04-structs-and-arrays.cpp’

---
+++ lectures/lect10/P04-structs-and-arrays.cpp
@@ -0,0 +1,30 @@
+/**
+ * @author Derek Harter
+ * @cwid   123 45 678
+ * @class  CSci 515, Spring 2015
+ * @ide    Emacs, GCC/G++
+ * @date   April 5, 2015
+ * @assg   Lecture 10, P04 Arrays of structures
+ *
+ * @description Examples of declaring arrays of structures and using
+ *    them as basic tables of data for processing.
+ */
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+
+// Data for a single experimental trial
+enum Gender {MALE, FEMALE};
+
+struct Trial
+{
+  string participantName;
+  Gender gender;
+  float reactionTime;  // in ms
+  int numberOfPresses; // number of times button was pressed
+};
+
+
